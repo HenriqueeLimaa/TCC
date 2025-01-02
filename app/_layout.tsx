bb4d11aa57ce2ea1ff653a99b479e8ae934987d8
HenriqueeLimaa <riquebarcelloslima@gmail.com>
2025-01-02 00:29:05 -0300

    Set the Auth pages as the application index to open it when the app is first opened

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -35,9 +35,10 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="loginPage" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="signIn" />
+        <Stack.Screen name="(tabs)" />
         <Stack.Screen name="pageNotFound" />
       </Stack>
       <StatusBar style="auto" />
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,46 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { StyleSheet, View } from "react-native";
+import { BodyText, PageContainer, Title } from "@/components/shared";
+import { AuthPageButton } from "@/components/signInAndSignUp";
 
-export default function LoginPage() {
+export default function AuthPage() {
+  const router = useRouter();
+  // idea: use some symbols of neurodivergencies, such as sunflower for autism or infinity for
+  // general neurodivergencies
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <PageContainer>
+      <Title>Welcome</Title>
+      <BodyText style={styles.instructionsText}>
+        Sign in or sign up to start to improve your productivity
+      </BodyText>
+      <View style={styles.imagePlaceholder} />
+      <View style={styles.buttonsContainer}>
+        <AuthPageButton onPress={() => router.push('/signIn')}>
+          Sign In
+        </AuthPageButton>
+        <AuthPageButton onPress={() => console.log("Sign Up Clicked!")}>
+          Sign Up
+        </AuthPageButton>
+      </View>
+    </PageContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  instructionsText: {
+    fontWeight: 'bold',
+    textAlign: "center",
+    maxWidth: "80%",
+  },
+  buttonsContainer: {
+    marginTop: 20,
+    gap: 10,
+  },
+  imagePlaceholder: {
+    backgroundColor: 'gray',
+    height: 300,
+    width: 300,
+    borderRadius: 10,
+    margin: 10,
+  },
+});
