dbebdbcb74dd965716119b0119906097726d124b
HenriqueeLimaa <riquebarcelloslima@gmail.com>
2025-01-02 04:55:20 -0300

    redirect user to the login page if theres no token

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,20 @@
-import { Tabs } from "expo-router";
+import { Tabs, useRouter } from "expo-router";
 import React from "react";
 import { Platform } from "react-native";
 import { HapticTab } from "@/components/HapticTab";
 import { IconSymbol } from "@/components/ui/IconSymbol";
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
-import { useColorScheme } from "@/hooks";
+import { useColorScheme, useLoginState } from "@/hooks";
 
 export default function TabLayout() {
+  const { accessToken } = useLoginState();
+  const router = useRouter();
+
+  if (!accessToken) {
+    router.push("/signIn");
+  }
+  
   const colorScheme = useColorScheme();
   return (
     <Tabs
