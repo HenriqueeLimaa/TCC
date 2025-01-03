975283bb67ff7d58579098f1f7dda2b5ab18c1de
HenriqueeLimaa <riquebarcelloslima@gmail.com>
2025-01-03 07:58:43 -0300

    Add i18n to use dynamic strings according to user locale

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,18 @@
-import { Tabs, useRouter } from "expo-router";
 import React, { useEffect } from "react";
 import { Platform } from "react-native";
+import { Tabs, useRouter } from "expo-router";
+import { useTranslation } from 'react-i18next';
+import { useColorScheme, useLoginState } from "@/hooks";
 import { HapticTab } from "@/components/HapticTab";
 import { IconSymbol, IconSymbolName } from "@/components/ui/IconSymbol";
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
-import { useColorScheme, useLoginState } from "@/hooks";
 
 export default function TabLayout() {
-  const { accessToken } = useLoginState();
   const router = useRouter();
   const colorScheme = useColorScheme();
+  const { accessToken } = useLoginState();
+  const { t } = useTranslation();
 
   useEffect(() => {
     if (!accessToken) {
@@ -41,7 +43,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="home"
         options={{
-          title: "Home",
+          title: t('tabBar.home'),
           tabBarIcon: ({ color }) => (
             <IconSymbol size={28} name="house.fill" color={color} />
           ),
@@ -61,7 +63,9 @@ export default function TabLayout() {
         options={{
           title: "Profile",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name={"accountCircle.fill" as IconSymbolName} color={color} />
+            // need to fix this later to show the correct icon, which could be the commented one
+            // <IconSymbol size={28} name={"accountCircle.fill" as IconSymbolName} color={color} />
+            <IconSymbol size={28} name="paperplane.fill" color={color} />
           ),
         }}
       />
---
+++ i18n.js
@@ -0,0 +1,36 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import * as Localization from 'expo-localization';
+import enUS from './locales/en_US.json';
+import ptBR from './locales/pt_BR.json';
+
+
+const resources = {
+  en: { translation: enUS },
+  pt: { translation: ptBR },
+};
+
+const languageDetector = {
+  type: 'languageDetector',
+  async: true,
+  detect: (callback) => {
+    const locales = Localization.getLocales();
+    const languageTag = locales && locales[0] ? locales[0].languageTag : 'en';
+    callback(languageTag);
+  },
+  init: () => {},
+  cacheUserLanguage: () => {},
+};
+
+i18n
+  .use(languageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
