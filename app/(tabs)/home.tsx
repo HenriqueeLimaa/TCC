73903894eacf20b24143eee2efaf3f4b7b67f2a4
HenriqueeLimaa <riquebarcelloslima@gmail.com>
2025-01-05 09:52:41 -0300

    Move logout button to profile tab

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,28 +1,10 @@
 import { PageContainer, Subtitle, Title } from "@/components/shared";
-import { useLoginState } from "@/hooks";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useRouter } from "expo-router";
-import { Button, StyleSheet, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function HomeScreen() {
-  const router = useRouter();
-  const { setUsername, setName, setEmail, setPassword, setAccessToken } =
-    useLoginState();
-
-  const handleLogout = () => {
-    setUsername("");
-    setName("");
-    setEmail("");
-    setPassword("");
-    setAccessToken("");
-    AsyncStorage.removeItem("accessToken");
-
-    router.replace("/signIn");
-  };
   return (
     <PageContainer>
       <Title>Home</Title>
-      <Button title="Logout" onPress={handleLogout} />
       <View style={styles.contentContainer}>
         <Subtitle>Add your goals</Subtitle>
       </View>
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,28 @@
-import { StyleSheet } from "react-native";
+import { Button, StyleSheet } from "react-native";
 import { PageContainer, Title } from "@/components/shared";
+import { useRouter } from "expo-router";
+import { useLoginState } from "@/hooks";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Profile() {
+  const router = useRouter();
+  const { setUsername, setName, setEmail, setPassword, setAccessToken } =
+    useLoginState();
+
+  const handleLogout = () => {
+    setUsername("");
+    setName("");
+    setEmail("");
+    setPassword("");
+    setAccessToken("");
+    AsyncStorage.removeItem("accessToken");
+
+    router.replace("/signIn");
+  };
   return (
     <PageContainer>
       <Title>Profile</Title>
+      <Button title="Logout" onPress={handleLogout} />
     </PageContainer>
   );
 }
